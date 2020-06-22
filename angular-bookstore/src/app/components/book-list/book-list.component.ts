edf72874de384f934613c467b7f237427f4b074a
Pio <piotrzogata@gmail.com>
2020-06-22 11:57:39 +0200

    Completed feature 3 search by category

--- angular-bookstore/src/app/components/book-list/book-list.component.ts
+++ angular-bookstore/src/app/components/book-list/book-list.component.ts
@@ -1,58 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { Book } from 'src/app/common/book';
 import { BookService } from 'src/app/services/book.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-book-list',
-  templateUrl: './book-list.component.html',
+  // templateUrl: './book-list.component.html',
+  templateUrl: './book-grid.component.html',
   styleUrls: ['./book-list.component.css']
 })
 export class BookListComponent implements OnInit {
 
-  constructor(private _bookService: BookService) { }
+  currentCategoryId: number;
+  books: Book[];
+
+  constructor(private _bookService: BookService, 
+    private _activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.listBooks()
+    //this.listBooks()
+    this._activatedRoute.paramMap.subscribe(()=>{
+      this.listBooks();
+    })
   }
-   books: Book[];// = [
-  //   {
-  //     sku: "text-100",
-  //     name: "Python for SAS Users",
-  //     description: "Business users familiar with Base SAS programming can now learn Python by example. You will learn via examples that map SAS programming constructs and coding patterns into their Python equivalents.",
-  //     unitPrice: 600,
-  //     imageUrl: "assets/images/webdevelopment/text-100.jpg",
-  //     active: true,
-  //     unitsInStock: 100,
-  //     createdOn: new Date(),
-  //     updatedOn: null,
-  //   },
-  //   {
-  //     sku: "text-101",
-  //     name: "Deep Learning with JavaScript",
-  //     description: "Deep learning has transformed the fields of computer vision, image processing, and natural language applications.",
-  //     unitPrice: 500,
-  //     imageUrl: "assets/images/webdevelopment/text-101.jpg",
-  //     active: true,
-  //     unitsInStock: 100,
-  //     createdOn: new Date(),
-  //     updatedOn: null,
-  //   },
-  //   {
-  //     sku: "text-102",
-  //     name: "Clean Ruby",
-  //     description: "Learn how to make better decisions and write cleaner Ruby code. This book shows you how to avoid messy code that is hard to test",
-  //     unitPrice: 700,
-  //     imageUrl: "assets/images/webdevelopment/text-102.jpg",
-  //     active: true,
-  //     unitsInStock: 100,
-  //     createdOn: new Date(),
-  //     updatedOn: null,
-  //   }
-  // ]
 
 
   listBooks(){
-    this._bookService.getBooks().subscribe(
+
+    const hasCategoryId: boolean = this._activatedRoute.snapshot.paramMap.has('id');
+
+    if(hasCategoryId){
+      this.currentCategoryId = +this._activatedRoute.snapshot.paramMap.get('id');
+    }else{
+      this.currentCategoryId = 1;
+    }
+
+
+    this._bookService.getBooks(this.currentCategoryId).subscribe(
       data => this.books = data
 
         //{console.log(data);}
